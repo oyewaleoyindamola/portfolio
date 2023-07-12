b52fb006dad7900e6c25a86de7089bd9a19ee691
oyewaleoyindamola <oyewaleoyindamola@gmail.com>
2023-07-12 01:17:04 -0700

    the fourth

--- src/Components/Plan.jsx
+++ src/Components/Plan.jsx
@@ -1,27 +1,91 @@
-import React from 'react'
+import React from "react";
+import tick from "../Assets/tick.svg";
 
 const Plan = () => {
   return (
     <div>
-        <div className=' flex flex-col justify-center items-center gap-7'>
-            <div className=' flex flex-col justify-center items-center w-full h-96 bg-[#EBEDFF]'>
-                <h1 className=' font-bold text-2xl mt-[-6rem]'>Without Extra Charge Choose </h1>
-                <h1 className=' font-bold text-2xl mb-8'>Your Best Plan</h1>
-                <div className=' h-16 w-52 flex gap-4 justify-center items-center rounded-md bg-white mt-'>
-                    <button className='bg-[#FF5A05] rounded-md h-10 w-20 px-2 ml-4 text-white'>Monthly</button>
-                    <button className='bg-[#FF5A051A] rounded-md h-10 w-20 px-2 mr-4 text-[#FF5A05]'>Yearly</button>
-                </div>
-            </div>
-            <div>
-                <div className=' bg-green-600 h-[20rem] w-[20rem] flex flex-col items-center'>
-                    <div className=' bg-[#1F37FF] h-10 w-16 rounded-b-[389px]'>
-
-                    </div>
-                </div>
-            </div>
+      <div className=" flex flex-col justify-center items-center gap-7">
+        <div className=" flex flex-col justify-center items-center w-full h-96 bg-[#EBEDFF]">
+          <h1 className=" font-bold text-2xl mt-[-6rem]">
+            Without Extra Charge Choose{" "}
+          </h1>
+          <h1 className=" font-bold text-2xl mb-8">Your Best Plan</h1>
+          <div className=" h-16 w-52 flex gap-4 justify-center items-center rounded-md bg-white mt-">
+            <button className="bg-[#FF5A05] rounded-md h-10 w-20 px-2 ml-4 text-white">
+              Monthly
+            </button>
+            <button className="bg-[#FF5A051A] rounded-md h-10 w-20 px-2 mr-4 text-[#FF5A05]">
+              Yearly
+            </button>
+          </div>
         </div>
+        <div className=" flex flex-row gap-10">
+          <div className=" bg-white h-[20rem] w-[20rem] flex flex-col items-center mt-[-8rem]">
+            <div className=" bg-[#1F37FF] h-10 w-16 rounded-b-[389px]"></div>
+            <h1 className=" font-bold text-2xl">Standard</h1>
+                <h1 className=" text-[#1F37FF] font-bold text-[4rem]">$29</h1>
+                <p>monthly pack</p>
+                <div>
+                    <div className=" flex gap-1">
+                    <img src={tick} alt="" />
+                    <p>256 GB SSD STORE</p>
+                </div>
+                <div className=" flex gap-1">
+                    <img src={tick} alt="" />
+                    <p>FULL UI KIT DOWNLOAD</p>
+                </div>
+                <div className=" flex gap-1">
+                    <img src={tick} alt="" />
+                    <p>PUBLIC API</p>
+                </div>
+                <div className=" flex gap-1">
+                    <img src={tick} alt="" />
+                    <p>FULL UI PACK DOWNLOAD</p>
+                </div>
+                <div className=" flex gap-1">
+                    <img src={tick} alt="" />
+                    <p>24/7 SUPPORT</p>
+                </div>
+                <button>
+                    <p>Install Pack</p>
+                </button>
+            </div>
+          </div>
+            <div className=" bg-white h-[20rem] w-[18rem] flex flex-col items-center mt-[-8rem]">
+                <div className=" bg-[#1F37FF] h-10 w-16 rounded-b-[389px]"></div>
+                <h1 className=" font-bold text-2xl">Standard</h1>
+                <h1 className=" text-[#1F37FF] font-bold text-[4rem]">$39</h1>
+                <p>monthly pack</p>
+                <div>
+                    <div className=" flex gap-1">
+                    <img src={tick} alt="" />
+                    <p>256 GB SSD STORE</p>
+                </div>
+                <div className=" flex gap-1">
+                    <img src={tick} alt="" />
+                    <p>FULL UI KIT DOWNLOAD</p>
+                </div>
+                <div className=" flex gap-1">
+                    <img src={tick} alt="" />
+                    <p>PUBLIC API</p>
+                </div>
+                <div className=" flex gap-1">
+                    <img src={tick} alt="" />
+                    <p>FULL UI PACK DOWNLOAD</p>
+                </div>
+                <div className=" flex gap-1">
+                    <img src={tick} alt="" />
+                    <p>24/7 SUPPORT</p>
+                </div>
+                <button>
+                    <p>Install Pack</p>
+                </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Plan
+export default Plan;
--- src/Components/UserReview.jsx
+++ src/Components/UserReview.jsx
@@ -1,41 +1,53 @@
-import React from 'react'
-import theMan from '../Assets/theMan.svg'
-import stars from '../Assets/stars.svg'
+import React from "react";
+import theMan from "../Assets/theMan.svg";
+import stars from "../Assets/stars.svg";
 const UserReview = () => {
   return (
-    <div className=' mb-20'>
-        <div className=' flex flex-col justify-center items-center mt-8'>
-            <h1 className=' font-bold text-3xl mb-2'> User's Review</h1>
-            <p >Collaborate over projects with your team and clients optimised</p>
-            <p className=' mb-10'>for mobile and tablet don't let slow</p>
-        </div>
-        <div className=' flex  justify-center gap-20'>
-            <div className=' ml-3'>
-                <h1 className=' ml-8 mt-5 font-bold text-2xl'> What People Say About </h1>
+    <div className=" mb-20">
+      <div className=" flex flex-col justify-center items-center mt-8">
+        <h1 className=" font-bold text-3xl mb-2"> User's Review</h1>
+        <p>Collaborate over projects with your team and clients optimised</p>
+        <p className=" mb-10">for mobile and tablet don't let slow</p>
+      </div>
+      <div className=" flex  justify-center gap-4">
+        <div className=" ml-3">
+          <h1 className=" ml-8 mt-5 font-bold text-2xl">
+            {" "}
+            What People Say About{" "}
+          </h1>
 
-                <div>
-                   <div className=' h-16 ml-8 mt-5 shadow-md py-3 px-5'>
-                      <h1 className=' font-bold'>Davit millar</h1>
-                      <p>CEO, PParkivew int, Ltd</p>
-                    </div>  
-                   <div className=' ml-52 mt-5 shadow-md h-32'>
-                      <img src={stars} alt=""  className=' ml-[10rem] w-48'/>
-                      <br />
-                      <p className=' '>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit nisl,<br /> 
-                        tincidunt commodo sit. Et, purus lectus odio cursus placerat leo, <br />
-                        non etiam. Neque at leo risus orci, 
-                      </p>
-                   </div>
-                </div>
-            </div>  
-
-            <div className=' ml-16'>
-                <img src={theMan} alt="" className=' w-96 h-96 ml-16'/>
+          <div className=" mt-14 w-[28rem] shadow-md h-[10rem] bg-white ">
+            <div className=" flex justify-between">
+              <div className="  h-16 w-[15rem] mt-5 shadow-md">
+              <h1 className=" font-bold">Davit millar</h1>
+              <p>CEO, PParkivew int, Ltd</p>
+              </div>
+            
+              <div>
+              <img src={stars} alt="" className=" w-48" />
+              </div>
+              </div>
+          
+            <div>
+             
+              <br />
+              <p className=" ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit
+                nisl,
+                <br />
+                tincidunt commodo sit. Et, purus lectus odio cursus placerat
+               
+              </p>
             </div>
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default UserReview
+        <div className=" -z-40">
+          <img src={theMan} alt="" className=" w-96 h-96 ml-[-4.5rem]" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserReview;
